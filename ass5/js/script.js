e529d5201c91841d941fee5e5aa9b72d0be704ba
swatinegi99 <swatinegii99@gmail.com>
2020-05-18 11:47:25 +0530

    mod

--- ass5/js/script.js
+++ ass5/js/script.js
@@ -116,7 +116,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       chosenCategoryShortName= "'" + chosenCategoryShortName + "'" ;
-       var homeHtmlToInsertIntoMainPage = insertproperty(homeHtml , 'randomCategoryShortName', chosenCategoryShortName);
+       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml , 'randomCategoryShortName', chosenCategoryShortName);
 
       
 
